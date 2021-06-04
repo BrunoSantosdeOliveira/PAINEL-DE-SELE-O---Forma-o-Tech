c47ceac0925ee186e8ede0da697aedcfd8f14dcf
BrunoSantosdeOliveira <84398016+BrunoSantosdeOliveira@users.noreply.github.com>
2021-06-04 00:26:50 -0300

    finalizacoes

--- custom.js
+++ custom.js
@@ -1,1 +1,6 @@
-var form = document.getElementById("formulario");
+function mockSendForm() {
+    isValidForm = (document.getElementById("nome").checkValidity() && document.getElementById("CPF").checkValidity() && document.getElementById("celular").checkValidity()&& document.getElementById("Endereco").checkValidity())
+    if (isValidForm) {
+        document.getElementById("mensagem_sucesso").style.display = "block";
+    }
+}
